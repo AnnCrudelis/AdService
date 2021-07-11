9ff9929544814e9443e77a0c83edacdf8fcd7194
anncrudelis <ann.crudelis@gmail.com>
2021-07-11 16:19:10 +0600

    Еще тесты

--- ApiTest/UnitTest1.cs
+++ ApiTest/UnitTest1.cs
@@ -7,6 +7,8 @@ using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using System.Transactions;
 using System.Net.Http;
+using WebApi.Filter;
+using WebApi.Wrappers;
 
 namespace ApiTest
 {
@@ -20,32 +22,28 @@ namespace ApiTest
         }
 
         [Fact]
-        public async Task Get_ShouldReturnListResult()
+        public async Task Get_ShouldReturnList()
         {
-            var response = await _fixture.Client.GetAsync("/api/ad");
-            response.EnsureSuccessStatusCode();
-            var models = JsonConvert.DeserializeObject<IEnumerable<Ad>>(await response.Content.ReadAsStringAsync());
+            var response = await _fixture.Client.GetAsync("/api/ad?pageNumber=1&pageSize=10&orderBy=Price");
+            var model = JsonConvert.DeserializeObject<List<PagedResponse<Ad>>>(await response.Content.ReadAsStringAsync());
 
-            Assert.NotEmpty(models);
-        }
-        [Fact]
-        public async Task Get_ShouldReturnListResultWithSort()
-        {
-            var response = await _fixture.Client.GetAsync("/api/ad/Price/true");
-            response.EnsureSuccessStatusCode();
-            var models = JsonConvert.DeserializeObject<IEnumerable<Ad>>(await response.Content.ReadAsStringAsync());
-
-            Assert.NotEmpty(models);
+            Assert.NotNull(model);
         }
 
         [Fact]
-        public async Task GetById_ShouldReturnOneResult()
+        public async Task Get_ShouldReturnOk()
         {
-            var response = await _fixture.Client.GetAsync("/api/ad/1");
-            response.EnsureSuccessStatusCode();
-            var models = JsonConvert.DeserializeObject<Ad>(await response.Content.ReadAsStringAsync());
+            var response = await _fixture.Client.GetAsync("/api/ad?pageNumber=1&pageSize=10&orderBy=Price");
 
-            Assert.NotNull(models);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnOk()
+        {
+            var response = await _fixture.Client.GetAsync("/api/ad/2");
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
@@ -76,6 +74,31 @@ namespace ApiTest
             var response = await _fixture.Client.PostAsync("api/ad", httpContent);
 
             Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ReturnsHttpCodeOk()
+        {
+            var responseDelete = await _fixture.Client.DeleteAsync("api/ad/1");
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, responseDelete.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ReturnsHttpCode404()
+        {
+            var responseDelete = await _fixture.Client.DeleteAsync("api/ad/100");
+
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, responseDelete.StatusCode);
+        }
+
+        [Fact]
+        public async void DeleteAndGet_ReturnsHttpCode404()
+        {
+            var responseDelete = await _fixture.Client.DeleteAsync("api/ad/3");
+            var responseGet = await _fixture.Client.GetAsync("/api/ad/3");
+
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, responseGet.StatusCode);
         }
 
     }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -61,6 +61,14 @@ namespace WebApi
             if (_currentEnvironment.IsEnvironment("Testing"))
             {
                 services.AddDbContextPool<AdContext>(options => options.UseInMemoryDatabase("TestingDB"));
+                services.AddHttpContextAccessor();
+                services.AddSingleton<IUriService>(o =>
+                {
+                    var accessor = o.GetRequiredService<IHttpContextAccessor>();
+                    var request = accessor.HttpContext.Request;
+                    var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+                    return new UriService(uri);
+                });
 
             }
             else
